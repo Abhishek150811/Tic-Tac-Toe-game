376c72dcc0e086ac5d683a3b1b7a8c9277e9847e
Abhishek150811 <abhishekaps2003@gmail.com>
2023-12-26 01:01:34 +0530

    First day of making of tic tac toe

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({name , symbol}){
+    const [old_name , set_newname] = useState(name) ;
+    const [curr_value , set_newvalue] = useState(false) ;
+
+    function handleClick(){
+        set_newvalue((prev_value) => !prev_value) ;
+    }
+    function handleChange(event){
+        console.log(event) ;
+        set_newname(event.target.value) ;
+    }
+
+    let playerName = <span className="player-name">{old_name} </span>
+    let btnValue = "Edit" ;
+    if(curr_value){
+        playerName = <input type="text" required value={old_name} onChange={handleChange}></input>
+        btnValue = "Save"
+    }
+    return(
+        <>
+         <li>
+            <span className="player">
+              {playerName}
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick} >{btnValue}</button>
+          </li>
+        </>
+    ) ;
+};
